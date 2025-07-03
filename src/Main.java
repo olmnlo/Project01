290e337887ee2d54b291ad49f109131790f544ce
olmnlo <hosamag54@gmail.com>
2025-07-03 11:37:15 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        runGame();
+    }
 
-        }
+    public static void printWelcomeMsg(){
+
+        System.out.println("""
+                
+                
+                 __      __   _                    _         _   _      _             _           \s
+                 \\ \\    / /__| |__ ___ _ __  ___  | |_ ___  | |_(_)__  | |_ __ _ __  | |_ ___  ___\s
+                  \\ \\/\\/ / -_) / _/ _ \\ '  \\/ -_) |  _/ _ \\ |  _| / _| |  _/ _` / _| |  _/ _ \\/ -_)
+                   \\_/\\_/\\___|_\\__\\___/_|_|_\\___|  \\__\\___/  \\__|_\\__|  \\__\\__,_\\__|  \\__\\___/\\___|
+                
+                
+                """);
+
+    }
+
+    public static void runGame(){
+        printWelcomeMsg();
+
+
     }
 }
