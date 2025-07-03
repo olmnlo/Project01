c1a957a2c9c55023de27b41134fbe24e865c581b
olmnlo <hosamag54@gmail.com>
2025-07-03 13:35:17 +0300

    make player can place his "X" in any place

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.desktop.SystemEventListener;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,8 +13,8 @@ public class Main {
         Random rand = new Random();
 
         String[][] game_board = {
-                {"1", "2", "3"},
-                {"4", "5", "6"},
+                {"", "", ""},
+                {"", "", "6"},
                 {"7", "8", "9"}
         };
 
@@ -21,17 +22,19 @@ public class Main {
 //        int who_play = rand.nextInt(2);
         int who_play = 0;
         while (round > 0){
-            printBoard(game_board, who_play);
-            switch (who_play){
-                case 0:
-                    game_board = player(game_board);
-                    break;
-                case 1:
+            while (true) {
+                printBoard(game_board, who_play);
+                switch (who_play) {
+                    case 0:
+                        game_board = player(game_board);
+                        break;
+                    case 1:
 //                    computer();
-                    System.out.println();
+                        System.out.println();
+                }
             }
 
-            round--;
+//            round--;
         }
 
 
@@ -76,14 +79,106 @@ public class Main {
 
 
     public static String[][] player(String[][] game_board){
-        System.out.println("Enter where do you want to play: ");
-        System.out.println("""
-                1- left up corner\t|\t4- left mid row|\t7- left down row
-                2- center up row\t|\t5- center mid row\t|\t8- center down row
-                3- right up corner\t|\t6- right mid row\t|\t9- right down row
-                """);
+        Scanner scn = new Scanner(System.in);
+        boolean played = true;
+        while (played) {
+            System.out.println("Enter where do you want to play: ");
+            System.out.println("""
+                    1- left up corner\t|\t4- left mid row|\t7- left down row
+                    2- center up row\t|\t5- center mid row\t|\t8- center down row
+                    3- right up corner\t|\t6- right mid row\t|\t9- right down row
+                    """);
+            try {
+                int player_chose = scn.nextInt();
+                switch (player_chose) {
+                    case 1:
+                        if (checkIsEmptyPlace(game_board, 0, 0)) {
+                            game_board[0][0] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 2:
+                        if (checkIsEmptyPlace(game_board, 0, 1)) {
+                            game_board[0][1] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 3:
+                        if (checkIsEmptyPlace(game_board, 0, 2)) {
+                            game_board[0][2] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 4:
+                        if (checkIsEmptyPlace(game_board, 1, 0)) {
+                            game_board[1][0] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 5:
+                        if (checkIsEmptyPlace(game_board, 1, 1)) {
+                            game_board[1][1] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 6:
+                        if (checkIsEmptyPlace(game_board, 1, 2)) {
+                            game_board[1][2] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 7:
+                        if (checkIsEmptyPlace(game_board, 2, 0)) {
+                            game_board[2][0] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 8:
+                        if (checkIsEmptyPlace(game_board, 2, 1)) {
+                            game_board[2][1] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    case 9:
+                        if (checkIsEmptyPlace(game_board, 2, 2)) {
+                            game_board[2][2] = "X";
+                            played = false;
+                        } else {
+                            System.out.println("It is not empty place you can not put your X here");
+                        }
+                        break;
+                    default:
+                        System.out.println("You must chose from menu numbers only");
 
-        return null;
+                }
+            }catch (Exception e){
+                scn.nextLine();// empty the scanner buffer
+                System.err.println("You cannot enter anything not Integer");
+            }
+
+        }
+
+        return game_board;
+    }
+
+    public static boolean checkIsEmptyPlace(String[][] game_board, int indexi, int indexj){
+        return game_board[indexi][indexj].isEmpty();
     }
 
 
@@ -101,17 +196,18 @@ public class Main {
                 int user_chose = scn.nextInt();
                 switch (user_chose) {
                     case 1:
+                        scn.nextLine();
                         return 1;
                     case 2:
+                        scn.nextLine();
                         return 3;
                     case 3:
+                        scn.nextLine();
                         return -1;
                 }
             } catch (Exception e) {
                 scn.nextLine();// empty the scanner buffer
                 System.err.println("You must enter integer number");
-            }finally {
-                scn.close();
             }
         }
     }
