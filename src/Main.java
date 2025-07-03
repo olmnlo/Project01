fc9315d6f6244a424d329efcf73744fed33e48ba
olmnlo <hosamag54@gmail.com>
2025-07-03 12:55:35 +0300

    add three functions Welcome board, print the board of game, how many times to play

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         runGame();
+    }
+
+    public static void runGame(){
+        printWelcomeMsg();
+        String[][] game_board = {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}
+        };
+        int round = showMenuAndChose();
+
+        while (round > 0){
+            printBoard(game_board);
+
+            round--;
+        }
+
+
     }
 
     public static void printWelcomeMsg(){
@@ -18,9 +38,46 @@ public class Main {
 
     }
 
-    public static void runGame(){
-        printWelcomeMsg();
 
 
+    public static void printBoard(String[][] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+                if (j%5 == 0 || j%5 != 0) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            System.out.println("----------");
+        }
+    }
+
+
+
+    //Bonus idea:
+    public static int showMenuAndChose(){
+        Scanner scn = new Scanner(System.in);
+        while (true) {
+            System.out.println("""
+                    1- one round win
+                    2- three rounds win
+                    3- exit
+                    """);
+            try {
+                int user_chose = scn.nextInt();
+                switch (user_chose) {
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 3;
+                    case 3:
+                        return -1;
+                }
+            } catch (Exception e) {
+                scn.nextLine();// empty the scanner buffer
+                System.err.println("You must enter integer number");
+            }
+        }
     }
 }
