4620f87e7fcb1d09048e6188a1d018ebbb22a351
olmnlo <hosamag54@gmail.com>
2025-07-03 19:46:19 +0300

    change the checkDiagonally to loops and make code shorter: optimized code

--- src/Main.java
+++ src/Main.java
@@ -309,9 +309,9 @@ public class Main {
             total_O = 0;
             total_X = 0;
             for (int i = 0; i < board_game.length; i++) {
-                if (board_game[i][board_game.length -1  - i].equals("X")) {
+                if (board_game[i][board_game.length - 1  - i].equals("X")) {
                     total_X++;
-                } else if (board_game[i][board_game.length -1 - i].equals("O")) {
+                } else if (board_game[i][board_game.length - 1 - i].equals("O")) {
                     total_O++;
                 }
             }
